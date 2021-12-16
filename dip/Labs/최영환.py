1a0a66e26b4c2d46f0c9a65983aa3584d221b729
dudghks97 <yh9638@skuniv.ac.kr>
2021-12-17 00:51:23 +0900

    12.17

--- dip/Labs/최영환.py
+++ dip/Labs/최영환.py
@@ -5,13 +5,55 @@ import os
 import pickle
 from matplotlib import pyplot as plt
 
+# used methods
+# =====================================================================================================================
+
+def compare_faces_ordered(encodings, face_names, encoding_to_check):
+    """Returns the ordered distances and names
+    when comparing a list of face encodings against a candidate to check"""
+    # 매칭값 순으로 나열하여 반환한다. ... 작은 값부터
+    # 매칭값에 따라 face_names 순서도 바꾸어 반환한다.
+
+    distances = list(np.linalg.norm(encodings - encoding_to_check, axis=1))
+    return zip(*sorted(zip(distances, face_names)))
+
+
+def compare_faces(encodings, encoding_to_check):
+    """Returns the distances when comparing a list of face encodings against a candidate to check"""
+
+    return list(np.linalg.norm(encodings - encoding_to_check, axis=1))
+    # linalg.norm 링크:
+    # https://numpy.org/doc/stable/reference/generated/numpy.linalg.norm.html
+
+def face_encodings(face_image, number_of_times_to_upsample=1, num_jitters=1):
+    """Returns the 128D descriptor for each face in the image"""
+
+    # Detect faces:
+    gray = cv2.cvtColor(face_image, cv2.COLOR_RGB2GRAY)
+    face_locations = detector(gray, number_of_times_to_upsample)
+
+    # Detected landmarks:
+    raw_landmarks = [pose_predictor_5_point(face_image, face_location) for face_location in face_locations]
+
+    # Calculate the face encoding for every detected face using the detected landmarks for each one:
+    return [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for
+            raw_landmark_set in raw_landmarks]
+
+# =====================================================================================================================
+
+# test image path & test image name list
 test_img_path = 'd:/dip/images/'
 test_file_list = ['t1.jpg', 't2.jpg', 't3.jpg', 't4.jpg', 't5.jpg']
 
+# model_path
 model_path = 'd:/dip/data/dlib_face_recog/'
 
+# shape predictor & recognition_model
 pose_predictor_5_point = dlib.shape_predictor(model_path + "shape_predictor_5_face_landmarks.dat")
 face_encoder = dlib.face_recognition_model_v1(model_path + "dlib_face_recognition_resnet_model_v1.dat")
+
+# dlib hog face detector
+detector = dlib.get_frontal_face_detector()
 
 # 런닝맨 멤버
 mb_han_lst = ['유재석', '지석진', '김종국', '하하', '송지효', '전소민', '양세찬']
@@ -24,17 +66,5 @@ threshold = 0.6
 print("threshold=", threshold)
 
 # dlib 얼굴 검출기 및 얼굴 인코더 로드
-with open("d:/dip/data/encoding_and_label.bin", "rb") as file:
-    with open("face_encoding_and_label.bin", "rb") as file:
-        all_member_face, all_member_encodings1d_lst, all_member1d_label_lst = pickle.load(file)
-
-for i, (a_dsc, a_lbl) in enumerate(zip(all_member_encodings1d_lst, all_member1d_label_lst)):
-    print(f"{i}: label={a_lbl}", end='')
-    for k in range(10):  # 한 사진의 인코딩 값을 앞의 10개만 찍어본다.
-        print(f"{a_dsc[k]:7.3f}", end=" ")
-    print()
-for i, (a_dsc, a_lbl) in enumerate(zip(all_member_encodings1d_lst, all_member1d_label_lst)):
-    print(f"{i}: label={a_lbl}", end='')
-    for k in range(10):  # 한 사진의 인코딩 값을 앞의 10개만 찍어본다.
-        print(f"{a_dsc[k]:7.3f}", end=" ")
-    print()
+with open(FileName, "rb") as file:
+    all_member_face, all_member_encodings1d_lst, all_member1d_label_lst = pickle.load(file)
